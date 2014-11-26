2a28687454339859a7a4cd0997fa3233a10ac2c9
Peter Prakobkit <kprakobkit@gmail.com>
2014-11-26 00:19:33 -0800

    implement function to show a page for viewing a recipe

--- public/js/app.js
+++ public/js/app.js
@@ -1,8 +1,10 @@
 $(document).ready(function(){
   $('form').submit(function(e) {
     e.preventDefault();
+    $('.recipe').hide();
     $('.loading').show();
-    $('.recipe').remove();
+    $('.recipe-link').remove();
+    $('.recipes').show();
     var data = $(this).serialize();
     var getRecipes = $.ajax({
       url: 'search',
@@ -13,8 +15,25 @@ $(document).ready(function(){
       $('.loading').hide();
       var recipes = data;
       recipes.forEach(function(recipeName) {
-        $('.recipes').append($("<p class='recipe'></p>").html(recipeName));
+        var recipeElement = document.createElement('p');
+        var linkElement = document.createElement('a');
+        recipeElement.className = 'recipe-link';
+        linkElement.innerHTML = recipeName;
+        linkElement.href='#';
+        linkElement.dataset.recipename = recipeName;
+        recipeElement.appendChild(linkElement);
+        $('.recipes').append(recipeElement);
       });
     });
+  });
+
+  $('.recipes').on('click', 'a', function(e) {
+    e.preventDefault();
+    $('.recipe').empty();
+    $('.recipes').hide();
+    $('div .recipe').show();
+    var headerElement = document.createElement('h3');
+    headerElement.innerHTML = this.dataset.recipename;
+    $('.recipe').append(headerElement);
   });
 });
